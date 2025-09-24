9813b93a2651e3b01d8c7e1079ab724f42ebfe65
Iverander <torje@iverander.com>
2025-09-24 21:07:46 +0200

    Rework incoming

--- Assets/!Scripts/Terminal/Syntax.cs
+++ Assets/!Scripts/Terminal/Syntax.cs
@@ -6,25 +6,50 @@ namespace Coding.Language
     public enum key
     {
         fail,
+
         Class,
+
         Void,
         Int,
         Float,
         String,
         Bool,
+
         If,
+        Else,
+
+        Return,
+        Continue,
     }
     public static class Syntax
     {
-        public static readonly Dictionary<key, string> keywords = new()
+        public static readonly Dictionary<key, Keyword> keywords = new()
         {
-            { key.Class, "class" },
-            { key.Void, "void" },
-            { key.Int, "int" },
-            { key.Float, "float" },
-            { key.String, "string" },
-            { key.Bool, "bool" },
-            { key.If, "if" },
+            { key.Class, new Keyword("class", Color.blue) },
+
+            { key.Void, new Keyword("void", Color.blue) },
+            { key.Int, new Keyword("int", Color.blue) },
+            { key.Float, new Keyword("float", Color.blue) },
+            { key.String, new Keyword("string", Color.blue) },
+            { key.Bool, new Keyword("bool", Color.blue) },
+
+            { key.If, new Keyword("if", Color.blue) },
+            { key.Else, new Keyword("else", Color.blue) },
+
+            { key.Return, new Keyword("return", Color.blue) },
+            { key.Continue, new Keyword("continue", Color.blue) },
         };
+
+        public struct Keyword
+        {
+            public string word;
+            public Color highlightColor;
+
+            public Keyword(string word, Color highlightColor)
+            {
+                this.word = word;
+                this.highlightColor = highlightColor;
+            }
+        }
     }
 }

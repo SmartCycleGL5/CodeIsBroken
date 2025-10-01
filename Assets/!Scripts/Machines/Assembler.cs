5695d044ab8a8acc0592bcb2cd4fd75ad8ad01eb
NoaTheSoap <noa.sjoset@gmail.com>
2025-10-01 08:02:05 +0200

    assembler

--- Assets/!Scripts/Machines/Assembler.cs
+++ Assets/!Scripts/Machines/Assembler.cs
@@ -1,8 +1,10 @@
+using System;
 using NUnit.Framework.Constraints;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class Assembler : MonoBehaviour, IItemContainer
+public class Assembler : Machine,IItemContainer
 {
     [SerializeField] int assemblerSize;
     [SerializeField] List<Item> items;
@@ -10,9 +12,17 @@ public class Assembler : MonoBehaviour, IItemContainer
 
     public Item item { get; set; }
 
-    public void Clear()
+    private void Start()
     {
-        items.Clear();
+        Tick.OnTick += TakeItem;
+        Tick.OnEndingTick += ClearMachine;
+    }
+
+    public override void Initialize(string initialClassName)
+    {
+        AddMethodsAsIntegrated(typeof(Assembler));
+
+        base.Initialize(initialClassName);
     }
     private void Update()
     {
@@ -23,7 +33,55 @@ public class Assembler : MonoBehaviour, IItemContainer
 
     public void Craft()
     {
+        if(items.Count <= 0) return;
+        // Materials of items currently held by conveyor
+        List<BaseMaterial> materials = new();
+        materials.AddRange(items.Select(i => i.material));
+        
+        //Loops over all recipes
+        foreach (var recipe in craftingRecipies)
+        {
+            // Sort lists to compare them:
+            materials = materials.OrderBy(x => x.type).ToList();
+            recipe.materials = recipe.materials.OrderBy(x => x.type).ToList();
+            
+            if (materials.SequenceEqual(recipe.materials))
+            {
+                ClearMachine();
+                Debug.Log("Valid recipe");
+                
+                // output item to next conveyor
+                GameObject cell = GridBuilder.instance.LookUpCell(transform.position + transform.forward);
+                if(cell == null) return;
+                if (cell.TryGetComponent(out Conveyor conveyor))
+                {
+                    conveyor.SetItem(Instantiate(recipe.itemToSpawn, transform.position, Quaternion.identity));
+                    RemoveItem();
+                }
+            }
+        }
+    }
 
+    [DontIntegrate]
+    private void TakeItem()
+    {
+        Debug.Log("TakeItem");
+        if(items.Count >= assemblerSize) return;
+        GameObject cell = GridBuilder.instance.LookUpCell(transform.position - transform.forward);
+        if (cell == null) return;
+        if (cell.TryGetComponent(out Conveyor conveyor))
+        {
+            item = conveyor.item;
+            if(item == null) return;
+            item.transform.position = transform.position;
+            items.Add(item);
+            conveyor.RemoveItem();
+        }
+    }
+
+    public void ClearMachine()
+    {
+        items.Clear();
     }
 
     public bool RemoveItem(out Item removedItem)
@@ -51,6 +109,7 @@ public class Assembler : MonoBehaviour, IItemContainer
 
     private void OnDestroy()
     {
-        
+        Tick.OnTick -= TakeItem;
+        Tick.OnEndingTick -= ClearMachine;
     }
 }

11487d5b6b2a9ac10282375fc9792d8405c63f0a
Iverander <43863123+Iverander@users.noreply.github.com>
2025-10-03 11:33:07 +0200

    more shit

---
+++ Assets/!Scripts/ShitJournal.cs
@@ -0,0 +1,34 @@
+using Coding;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UIManager;
+
+public class ShitJournal : MonoBehaviour, IWindow
+{
+
+    public VisualTreeAsset journalAsset { get; private set; }
+    public VisualTreeAsset journalElementAsset { get; private set; }
+    public UIManager.Window window { get; set; }
+
+    VisualElement journal;
+
+    private async void Start()
+    {
+        if (journalAsset == null)
+        {
+            journalAsset = await Utility.Addressable.ReturnAdressableAsset<VisualTreeAsset>("Window/ShitJournal");
+        }
+        if (journalElementAsset == null)
+        {
+            journalElementAsset = await Utility.Addressable.ReturnAdressableAsset<VisualTreeAsset>("Window/ShitJournalElement");
+        }
+
+        journal = journalAsset.Instantiate();
+        window = new Window("Journal", journal, this);
+    }
+
+    public void Close()
+    {
+        Destroy(this);
+    }
+}

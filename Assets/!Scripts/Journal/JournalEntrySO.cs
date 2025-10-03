6e23c3ba6a11d4b65d8fe9c164b53cdfd5ef2341
AskMake <103579240+AskMake@users.noreply.github.com>
2025-10-03 09:54:26 +0200

    Hint start not finished

--- Assets/!Scripts/Journal/JournalEntrySO.cs
+++ Assets/!Scripts/Journal/JournalEntrySO.cs
@@ -6,6 +6,7 @@ namespace Journal
     [CreateAssetMenu(fileName = "JournalEntrySO", menuName = "Scriptable Objects/JournalEntrySO")]
     public class JournalEntrySO : ScriptableObject
     {
+        public string title;
         public Texture2D showcaseI;
         public string explanation;
 
--- Assets/!Scripts/Journal/JournalManager.cs
+++ Assets/!Scripts/Journal/JournalManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -10,13 +11,12 @@ namespace Journal
     {
         public static JournalManager instance { get; private set; }
         public JournalEntrySO[] machineEntries;
-        public JournalEntrySO[] factoryEntries;
+        public JournalEntrySO[] HintEntries;
         public JournalEntrySO[] economyEntries;
         [SerializeField] string machineTabName = "Machine";
-        [SerializeField] string factoryTabName = "Factory";
+        [SerializeField] string hintsTabName = "Hints";
         [SerializeField] string economyTabName = "Economy";
         public UIDocument journalDoc;
-        [SerializeField] Texture2D image;
         void Awake()
         {
             if (!instance) { instance = this; }
@@ -24,35 +24,58 @@ namespace Journal
 
             journalDoc.rootVisualElement.style.display = DisplayStyle.None;
             machineEntries = Resources.LoadAll<JournalEntrySO>("Journal/Machines");
-            factoryEntries = Resources.LoadAll<JournalEntrySO>("Journal/Factory");
+            HintEntries = Resources.LoadAll<JournalEntrySO>("Journal/Hints");
             economyEntries = Resources.LoadAll<JournalEntrySO>("Journal/Economy");
 
         }
         void OnEnable()
         {
             AddEntry(machineEntries, machineTabName);
-            AddEntry(factoryEntries, factoryTabName);
+            AddEntry(HintEntries, hintsTabName);
             AddEntry(economyEntries, economyTabName);
         }
         void AddEntry(JournalEntrySO[] entries, string nameD)
         {
-            List<string> entryNames = entries.Select(obj => obj.name).ToList();
             var tab = journalDoc.rootVisualElement.Q<Tab>(nameD);
-            tab.tabHeader.style.backgroundImage = image;
-            var entryDrop = tab.Q<DropdownField>("Dropdown");
-            entryDrop.choices = entryNames;
-            entryDrop.RegisterValueChangedCallback(evt => DropDownChange(entries, tab, entryDrop.index));
-            entryDrop.index = 0;
+            var scrollView = tab.Q<ScrollView>();
+            foreach (var entry in entries)
+            {
+                if (entry.title != string.Empty && entry.explanation != string.Empty && entry.showcaseI != null)
+                {
+                    var tButton = new Button() { text = entry.title, style = { backgroundImage = entry.showcaseI } };
+                    tButton.RegisterCallback<MouseUpEvent>((evt) => changeEntry(tab, entry));
+                    scrollView?.Add(tButton);
+                }
+                else
+                {
+                    Debug.LogError("Missing Information for : " + entry.name + "\nButton Not Added");
+                }
+            }
+            // var entryDrop = tab.Q<Dropdown Field>("Dropdown");
+            // entryDrop.choices = entryNames;
+            // entryDrop.RegisterValueChangedCallback(evt => DropDownChange(entries, tab, entryDrop.index));
+            // entryDrop.index = 0;
         }
-        void DropDownChange(JournalEntrySO[] entries, Tab tab, int index)
+
+        private void changeEntry(Tab tab, JournalEntrySO entry)
         {
-            var machineImage = tab.Q<VisualElement>("Image");
-            var machineExText = tab.Q<Label>("Explanation");
-            var machineCodeText = tab.Q<Label>("Code");
-            machineImage.style.backgroundImage = entries[index].showcaseI;
-            machineExText.text = entries[index].explanation;
-            machineCodeText.text = entries[index].codeShowcase;
+            var explainT = tab.Q<Label>("Explanation");
+            var codeT = tab.Q<Label>("Code");
+            explainT.text = entry.explanation;
+            codeT.text = entry.codeShowcase;
         }
+
+
+
+        // void DropDownChange(JournalEntrySO[] entries, Tab tab, int index)
+        // {
+        //     var machineImage = tab.Q<VisualElement>("Image");
+        //     var machineExText = tab.Q<Label>("Explanation");
+        //     var machineCodeText = tab.Q<Label>("Code");
+        //     machineImage.style.backgroundImage = entries[index].showcaseI;
+        //     machineExText.text = entries[index].explanation;
+        //     machineCodeText.text = entries[index].codeShowcase;
+        // }
         public void JournalOnOff()
         {
             if (journalDoc.rootVisualElement.style.display == DisplayStyle.Flex)

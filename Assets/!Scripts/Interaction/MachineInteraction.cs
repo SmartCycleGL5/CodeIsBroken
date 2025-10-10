d66c1fcbaa7d5d6d5c36b33333ca165c8709e926
NoaTheSoap <noa.sjoset@gmail.com>
2025-10-10 14:39:34 +0200

    Better Selecting buildings

--- Assets/!Scripts/Interaction/MachineInteraction.cs
+++ Assets/!Scripts/Interaction/MachineInteraction.cs
@@ -13,27 +13,41 @@ public class MachineInteraction : MonoBehaviour
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             Debug.Log("[MachineInteration] Looking for object to toggle");
-            Vector2 mousePos = GridBuilder.instance.GetGridPosition();
-            GameObject building = GridBuilder.instance.LookUpCell(new Vector3(mousePos.x,0,mousePos.y));
-            if (building == null)
+            // Vector2 mousePos = GridBuilder.instance.GetGridPosition();
+            // GameObject building = GridBuilder.instance.LookUpCell(new Vector3(mousePos.x,0,mousePos.y));
+            // if (building == null)
+            // {
+            //     // If mouse clicks on empty space, and it is not the menu panel, close it.
+            //     if (currentMachineUI != null && !EventSystem.current.IsPointerOverGameObject())
+            //     { 
+            //         currentMachineUI.ToggleUI(false);
+            //         currentMachineUI = null;
+            //     }
+            //     return;
+            // }
+
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
             {
-                // If mouse clicks on empty space, and it is not the menu panel, close it.
-                if (currentMachineUI != null && !EventSystem.current.IsPointerOverGameObject())
-                { 
-                    currentMachineUI.ToggleUI(false);
-                    currentMachineUI = null;
+                GameObject building = GridBuilder.instance.LookUpCell(hit.collider.gameObject.transform.position);
+                if (building == null)
+                {
+                    // If mouse clicks on empty space, and it is not the menu panel, close it.
+                    if (currentMachineUI != null && !EventSystem.current.IsPointerOverGameObject())
+                    { 
+                        currentMachineUI.ToggleUI(false);
+                        currentMachineUI = null;
+                    }
+                    return;
                 }
-                return;
+                if (building.TryGetComponent(out MachineUIController machineUI)) 
+                {
+                    Debug.Log(machineUI.gameObject);
+                    SelectMachine(machineUI);
+                    Debug.Log("[MachineInteration] building: " + building);
+                }
             }
-
-            Debug.Log("[MachineInteration] building: " + building);
-
-            if (building.TryGetComponent(out MachineUIController machineUI)) 
-            {
-                Debug.Log(machineUI.gameObject);
-                SelectMachine(machineUI);
-            }
-
         }
     }
 

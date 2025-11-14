495e8f4bb4df346ea8016789ce9aba4d61521a66
NoaTheSoap <noa.sjoset@gmail.com>
2025-11-14 10:38:07 +0100

    Painter not working

--- Assets/!Scripts/Machines/PainterConveyor.cs
+++ Assets/!Scripts/Machines/PainterConveyor.cs
@@ -1,82 +1,77 @@
 using DG.Tweening;
 using UnityEngine;
-    public class PainterConveyor : MonoBehaviour, IItemContainer
+public class PainterConveyor : MonoBehaviour, IItemContainer
+{
+    Tweener moveTween;
+    public Item item { get; set; }
+
+
+
+    void Start()
     {
-        
-        [SerializeField] Transform input;
-        [SerializeField] Transform output;
-        [SerializeField] BaseMachine PainterMachine;
-        
-        Tweener moveTween;
-        public Item item { get; set; }
-    
-    
-    
-        void Start()
-        {
-            Tick.OnTick += TakeItem;
-        }
-    
-        private void TakeItem()
-        {
-            // Output item
-            GameObject outputCell = GridBuilder.instance.LookUpCell(transform.position+transform.forward);
-            if(outputCell == null) return;
-            if (outputCell.TryGetComponent(out Conveyor conveyorOut))
-            {
-                if (conveyorOut.item != null) return;
-                conveyorOut.SetItem(item);
-                RemoveItem();
-            }
-    
-            //Take in item
-            if (item != null) return;
-            GameObject inputCell = GridBuilder.instance.LookUpCell(transform.position - transform.forward);
-            if(inputCell == null) return;
-            if(inputCell.TryGetComponent(out Conveyor conveyorIn))
-            {
-                if(conveyorIn.item == null) return;
-                SetItem(conveyorIn.item);
-                conveyorIn.RemoveItem();
-            }
-    
-            //PainterMachine.item = item; we need a different solution lol
-        }
-    
-        public bool RemoveItem(out Item removedItem)
-        {
-            removedItem = null;
-            if (item == null) return false;
-            removedItem = item;
-            item = null;
-            if (moveTween != null)
-            {
-                moveTween.Kill();
-            }
-            return true;
-        }
-    
-        public bool SetItem(Item item)
-        {
-            if (this.item != null) return false;
-            this.item = item;
-            moveTween = this.item.gameObject.transform.DOMove(transform.position+new Vector3(0,1,0),0.3f);
-            return true;
-        }
-        [DontIntegrate]
-        public bool RemoveItem()
-        {
-            return RemoveItem(out Item item);
-        }
-    
-        private void OnDestroy()
-        { 
-            Tick.OnTick -= TakeItem;
-    
-            if (item == null) return;
-            Debug.Log("Removed item from painter");
-            Destroy(item.gameObject);
-            RemoveItem();
-            
-        }
+        Tick.OnTick += TakeItem;
     }
+
+    private void TakeItem()
+    {
+        // Output item
+        GameObject outputCell = GridBuilder.instance.LookUpCell(transform.position+transform.forward);
+        if(outputCell == null) return;
+        if (outputCell.TryGetComponent(out Conveyor conveyorOut))
+        {
+            if (conveyorOut.item != null) return;
+            conveyorOut.SetItem(item);
+            RemoveItem();
+        }
+
+        //Take in item
+        if (item != null) return;
+        GameObject inputCell = GridBuilder.instance.LookUpCell(transform.position - transform.forward);
+        if(inputCell == null) return;
+        if(inputCell.TryGetComponent(out Conveyor conveyorIn))
+        {
+            if(conveyorIn.item == null) return;
+            SetItem(conveyorIn.item);
+            conveyorIn.RemoveItem();
+        }
+
+        //PainterMachine.item = item; we need a different solution lol
+    }
+
+    public bool RemoveItem(out Item removedItem)
+    {
+        removedItem = null;
+        if (item == null) return false;
+        removedItem = item;
+        item = null;
+        if (moveTween != null)
+        {
+            moveTween.Kill();
+        }
+        return true;
+    }
+
+    public bool SetItem(Item item)
+    {
+        if (this.item != null) return false;
+        this.item = item;
+        moveTween = this.item.gameObject.transform.DOMove(transform.position+new Vector3(0,1,0),0.3f);
+        return true;
+    }
+    [DontIntegrate]
+    public bool RemoveItem()
+    {
+        return RemoveItem(out Item item);
+    }
+
+    private void OnDestroy()
+    { 
+        Tick.OnTick -= TakeItem;
+
+        if (item == null) return;
+        Debug.Log("Removed item from painter");
+        Destroy(item.gameObject);
+        RemoveItem();
+        
+    }
+}

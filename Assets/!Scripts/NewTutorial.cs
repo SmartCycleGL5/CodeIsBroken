31e95298713092dbd243ce5357c328bb2ffa3094
Iverander <torje@iverander.com>
2025-11-22 18:11:53 +0100

    bug fixes

--- Assets/!Scripts/NewTutorial.cs
+++ Assets/!Scripts/NewTutorial.cs
@@ -121,6 +121,8 @@ public class NewTutorial : MonoBehaviour
         }
     }
 
+
+    private Contract first;
     void ProgrammingTutorial()
     {
         switch (buildingIndex)
@@ -135,7 +137,8 @@ public class NewTutorial : MonoBehaviour
                 if (!subscribed)
                 {
                     Debug.Log("Subscribed");
-                    ContractManager.ActiveContract.onFinished += OnFinishedContract;
+                    first = ContractManager.ActiveContract;
+                    first.onFinished += OnFinishedContract;
                     subscribed = true;
                 }
                 break;
@@ -158,6 +161,6 @@ public class NewTutorial : MonoBehaviour
     {
         Debug.Log("Finished");
         buildingIndex++;
-        ContractManager.ActiveContract.onFinished -= OnFinishedContract;
+        first.onFinished -= OnFinishedContract;
     }
 }
